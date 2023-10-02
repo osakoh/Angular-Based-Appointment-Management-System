3508fa849e32af6f6efc27501ea63462d57e63ae
osakoh <macmive@gmail.com>
2023-10-02 11:21:38 +0100

    impld saving to local storage

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -23,16 +23,24 @@ export class AppointmentListComponent {
 
       // add newAppointment to end of appointments array
       this.appointments.push(newAppointment);
+
       // reset input fields
       this.newAppointmentTitle = '';
       this.newAppointmentDate = new Date();
+
+      // store in localstorage -> convert object to JSON then save to LS
+      localStorage.setItem('appointments', JSON.stringify(this.appointments));
     }
   }
 
   // delete appointment
   deleteAppointment(index: number, appointment: object) {
     this.appointments.splice(index, 1);
-    console.log('Deleted appointment index =>', index);
-    console.log('Deleted appointment object =>', appointment);
+
+    // resave entire appointment to LS(only use for small scale projects)
+    localStorage.setItem('appointments', JSON.stringify(this.appointments));
+
+    // console.log('Deleted appointment index =>', index);
+    // console.log('Deleted appointment object =>', appointment, '\n');
   }
 }
